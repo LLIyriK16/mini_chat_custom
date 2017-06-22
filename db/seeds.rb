9334f7b1ca226f4a7a7e9cdb4b21890fe60bb345
Andriy <krafter18@gmail.com>
2017-06-22 12:18:39 +0300

    Added comment to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-User.create(email:"bigboss@jojo.com", password:"123123", password_confirmation:"123123")
+@user = User.create(email: "bigboss@jojo.com", password: "123123", password_confirmation: "123123")
 
 puts "1 User created"
+
+99.times do |count|
+  @comments = Comment.create(body: "#{count} comment", user_id: @user.id)
+end
+
+puts "99 Comments created"
